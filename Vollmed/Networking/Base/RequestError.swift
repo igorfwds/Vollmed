ce6d151b6d83d0217b5f894035d027b01ece96ee
Igor Wanderley <igorfwds@hotmail.com>
2024-10-17 17:59:49 -0300

    manuseando erros em requisições swift

---
+++ Vollmed/Networking/Base/RequestError.swift
@@ -0,0 +1,28 @@
+//
+//  RequestError.swift
+//  Vollmed
+//
+//  Created by ifws on 17/10/24.
+//
+
+import Foundation
+
+enum RequestError: Error {
+    case decode
+    case invalidURL
+    case noResponse
+    case unauthorized
+    case unknown
+    case custom(_ error: [String: Any])
+    
+    var customMessage: String {
+        switch self {
+        case .decode:
+            return "erro de decodificação"
+        case .unauthorized:
+            return "sessão expirada"
+        default:
+            return "erro desconhecido"
+        }
+    }
+}
---
+++ Vollmed/Networking/Base/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  Vollmed
+//
+//  Created by ifws on 17/10/24.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
